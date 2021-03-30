aa59a86a6031096ce24acf99e843d10e1ff7514a
ncuxoxupypr <eu.kondratev@yandex.ru>
2021-03-30 09:46:07 +0300

    add

--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -1,9 +1,13 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Rabbit4 {
+
+    @Autowired
+    private Duck5 duck5;
 
     @Override
     public String toString() {

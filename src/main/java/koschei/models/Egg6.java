f5ed21ca980837e1b1938a37451e0c61cf17713d
ncuxoxupypr <eu.kondratev@yandex.ru>
2021-03-30 10:46:11 +0300

    add bean

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -4,14 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 public class Egg6 {
 
-    private Needle7 needle;
+    private Needle7 needle7;
 
     @Autowired
-    public Egg6(Needle7 needle) {
-        this.needle = needle;
+    public Egg6(Needle7 needle7) {
+        this.needle7 = needle7;
     }
     @Override
     public String toString() {
-        return ", в яйце иголка " + needle.toString();
+        return ", в яйце иголка " + needle7.toString();
     }
 }
--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -5,12 +5,15 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Rabbit4 {
+    private Duck5 duck5;
 
     @Autowired
-    private Duck5 duck5;
+   public void setDuck5(Duck5 duck5) {
+        this.duck5 = duck5;
+    }
 
     @Override
     public String toString() {
-        return ", в зайце утка " + "";
+        return ", в зайце утка " + duck5.toString();
     }
 }
